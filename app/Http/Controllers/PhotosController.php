f685370ebc2ee0b1465c174a93a525d20df5533a
Mazen Aboelanin <mazenaboelanin@gmail.com>
2019-07-03 18:05:40 +0200

    Album Photo Listings

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -51,4 +51,5 @@ class PhotosController extends Controller
       
 
     }
+
 }
--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -8,4 +8,42 @@
 
 <hr>
 
+@if( count($album->photos) > 0 )
+<?php
+    $colcount = count($album->photos);
+    $i = 1 ;
+?>
+<div id="photos">
+    <div class="row text-center">
+        @foreach($album->photos as $photo)
+            @if($i == $colcount)
+                <div class="col-md-4">
+                    <a href="/photos/{{$photo->id}}">
+                    <img style="height:50%; width:100%"  class="thumbnail" src="/storage/photos/{{$photo->album_id}}/{{$photo->photo}}" alt="{{$photo->title}}">
+                    </a>
+                    <br>
+                    <h4>{{$photo->title}}</h4>
+            @else 
+                <div class="col-md-4">
+                    <a href="/photos/{{$photo->id}}">
+                        <img style="height:50%; width:100%"  class="thumbnail" src="/storage/photos/{{$photo->album_id}}/{{$photo->photo}}" alt="{{$photo->title}}">
+                        </a>
+                        <br>
+                        <h4>{{$photo->title}}</h4>
+            
+            @endif
+
+            @if($i % 3 == 0)
+            </div></div><div class="row text-center">
+            @else
+            </div>
+            @endif
+            <?php $i++ ; ?>
+        @endforeach
+    </div>
+</div>
+@else 
+<br><br>
+<p>No Photo to Display</p>
+@endif
 @endsection
